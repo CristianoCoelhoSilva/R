2f218612747ce6f1df9421f75fc6c5889fe1de70
CristianoCoelhoSilva <62145164+CristianoCoelhoSilva@users.noreply.github.com>
2025-02-05 11:10:32 -0300

    Update Index.R

--- Index.R
+++ Index.R
@@ -1,21 +1,31 @@
 options(scipen = 999)
-library(sqldf)
 library(ggrepel)
 library(ggthemes)
 library(ggplot2)
 library(data.table)
+library(readxl)
+library(tidyr)
+library(dplyr)
 
-dados <- read.csv("./DADOS/Brasil2.csv")
+# Definir o caminho para o arquivo Excel
+arquivo_excel <- "DADOS/setores_brasil.xlsx"
 
-dados$Data <- as.Date(dados$data, format = "%Y-%m-%d")
-dados$indice <- as.numeric(gsub(",", ".", dados$indice))
-dados$valore <- as.factor(dados$valor)
+# Ler a planilha (caso você tenha mais de uma planilha, especifique o nome ou índice da planilha)
+dados <- read_excel(arquivo_excel)
 
-index <- ggplot(dados, aes(x = Data, y = indice)) +
+dados <- dados %>%
+  pivot_longer(cols = starts_with("MXBR"),  # Seleciona as colunas que começam com "MXBR"
+               names_to = "Indice",         # Cria a coluna "Indice" com o nome das colunas antigas
+               values_to = "Valor")        # Cria a coluna "Valor" com os valores
+
+dados$Data <- as.Date(dados$dates, format = "%Y-%m-%d")
+dados$Valor <- as.numeric(gsub(",", ".", dados$Valor))
+
+index <- ggplot(dados, aes(x = Data, y = Valor)) +
   geom_line(color = "blue", size = 0.5) +
   labs(title = "Série", x = "Data", y = "Valor") + 
   scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
-  facet_wrap(facets = ~valor, scales = "free_y") +  
+  facet_wrap(facets = ~Indice, scales = "free_y") +  
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(x="Data", y="Valor")
@@ -31,8 +41,7 @@ ggsave(
   dpi = 640
 )
 
-library(dplyr)
-library(ggplot2)
+
 
 # Calcular a diferença do logaritmo de 'indice'
 dados <- dados %>%
@@ -52,11 +61,11 @@ index <- ggplot(dados, aes(x = Data, y = diff_log_indice, color = valore)) +
 # Exibindo o gráfico
 print(index)
 
-# Calcular a diferença logarítmica por grupo na coluna 'name'
+# Calcular a diferença logarítmica por grupo na coluna 'Indice'
 dados <- dados %>%
-  arrange(name, Data) %>%  # Garantir que os dados estão ordenados por 'name' e 'Data'
-  group_by(name) %>%  # Agrupar os dados pela coluna 'name'
-  mutate(diff_log_indice = c(NA, diff(log(indice)))) %>%  # Calcula a diferença logarítmica dentro de cada grupo
+  arrange(Indice, Data) %>%  # Garantir que os dados estão ordenados por 'name' e 'Data'
+  group_by(Indice) %>%  # Agrupar os dados pela coluna 'name'
+  mutate(diff_log_indice = c(NA, diff(log(Valor)))) %>%  # Calcula a diferença logarítmica dentro de cada grupo
   ungroup()  # Desagrupar após a transformação
 
 # Plotando os dados com a transformação diff(log(indice)) por grupo 'name'
@@ -64,7 +73,7 @@ index <- ggplot(dados, aes(x = Data, y = diff_log_indice, color = name)) +
   geom_line(size = 0.5, color = "blue", ) +
   labs(title = "Série", x = "Data", y = "Diferença Log(Indice)") + 
   scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
-  facet_wrap(facets = ~name, scales = "free_y") +  
+  facet_wrap(facets = ~Indice, scales = "free_y") +  
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(x = "Data", y = "Diferença Log(Indice)")
